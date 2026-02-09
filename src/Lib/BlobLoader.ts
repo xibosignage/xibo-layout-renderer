dec99b39a88c5f2a3d93b4349b1d41dddf7ab57d
Ruben Pingol <ruben.pingol@xibosignage.com>
2026-02-09 17:04:26 +0800

    Add guard to use local server URL for electron platform

--- src/Lib/BlobLoader.ts
+++ src/Lib/BlobLoader.ts
@@ -1,3 +1,5 @@
+import {ConsumerPlatform} from "../Types/Platform";
+
 export class BlobLoader {
   // Map to store active Object URLs: { originalUrl: blobUrl }
   private static cache: Map<string, string> = new Map();
@@ -5,7 +7,7 @@ export class BlobLoader {
   /**
    * Fetches a video and returns a local Blob URL (e.g., blob:http://localhost/...)
    */
-  public static async load(url: string): Promise<string> {
+  public static async load(url: string, platform?: ConsumerPlatform): Promise<string> {
     // Return cached if exists
     if (this.cache.has(url)) {
       return this.cache.get(url)!;
@@ -15,8 +17,20 @@ export class BlobLoader {
       const response = await fetch(url);
       if (!response.ok) throw new Error(`Failed to fetch video: ${response.statusText}`);
 
-      const blob = await response.blob();
-      const objectUrl = URL.createObjectURL(blob);
+      let objectUrl = url;
+
+      if (!platform || (platform && platform !== ConsumerPlatform.ELECTRON)) {
+        const blob = await response.blob();
+        objectUrl = URL.createObjectURL(blob);
+      } else {
+        objectUrl = url;
+      }
+
+      console.debug('??? XLR.debug >> BlobLoader.load', {
+        url,
+        objectUrl,
+        response,
+      })
 
       this.cache.set(url, objectUrl);
       return objectUrl;
